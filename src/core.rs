f0b86de2d672bb43640883a1fcc0a958a6c927ec
Gregory Higley <greg@prosumma.com>
2025-02-22 00:16:15 -0500

    partial

---
+++ src/core.rs
@@ -0,0 +1,172 @@
+#[derive(Clone, Copy, PartialEq, Eq)]
+pub enum ParseError {
+  PartialMatch(usize),
+  NoMatch(usize),
+  End
+}
+
+pub use ParseError::*;
+
+pub struct ParseContext<'a, I> {
+  input: &'a [I],
+  pub position: usize
+}
+
+impl<'a, I> ParseContext<'a, I> {
+  pub fn new(input: &[I]) -> ParseContext<I> {
+    ParseContext { input, position: 0 }
+  }
+
+  pub fn current(&self) -> Option<&I> {
+    self.input.get(self.position)
+  }
+
+  pub fn err_partial_match(&self) -> ParseError {
+    PartialMatch(self.position)
+  }
+
+  pub fn err_no_match(&self) -> ParseError {
+    NoMatch(self.position)
+  }
+
+  pub fn throw_partial_match<O>(&self) -> Result<O, ParseError> {
+    Err(self.err_partial_match())
+  }
+
+  pub fn throw_no_match<O>(&self) -> Result<O, ParseError> {
+    Err(self.err_no_match())
+  }
+
+  pub fn throw_end<O>(&self) -> Result<O, ParseError> {
+    Err(End)
+  }
+}
+
+pub trait Parser<I, O>: Clone {
+  fn parse(&mut self, context: &mut ParseContext<I>) -> Result<O, ParseError>;
+
+  fn map<M>(self, f: impl FnMut(O) -> M + Clone) -> impl Parser<I, M> {
+    map(self, f)
+  }
+
+  fn partial(self, after: usize) -> impl Parser<I, O> {
+    partial(self, after)
+  }
+
+  fn or(self, second: impl Parser<I, O>) -> impl Parser<I, O> {
+    or(self, second)
+  }
+
+  fn many(self) -> impl Parser<I, Vec<O>> {
+    many(self)
+  }
+
+  fn preceded_by<F>(self, first: impl Parser<I, F>) -> impl Parser<I, O> {
+    second(first, self)
+  }
+
+  fn followed_by<S>(self, second: impl Parser<I, S>) -> impl Parser<I, O> {
+    first(self, second)
+  }
+}
+
+impl<I, O, F: Clone> Parser<I, O> for F
+  where F: FnMut(&mut ParseContext<I>) -> Result<O, ParseError>
+{
+  fn parse(&mut self, context: &mut ParseContext<I>) -> Result<O, ParseError> {
+    self(context)
+  }
+}
+
+pub fn satisfy<I: Clone>(mut test: impl FnMut(&I) -> bool + Clone) -> impl Parser<I, I> {
+  move |context: &mut ParseContext<I>| {
+    if let Some(i) = context.current() {
+      if test(i) {
+        let i = i.clone();
+        context.position += 1;
+        Ok(i)
+      } else {
+        context.throw_no_match()
+      }
+    } else {
+      context.throw_end()
+    }
+  }
+}
+
+pub fn map<I, O, M>(mut parser: impl Parser<I, O>, mut f: impl FnMut(O) -> M + Clone) -> impl Parser<I, M> {
+  move |context: &mut ParseContext<I>| {
+    parser.parse(context).map(&mut f)
+  }
+}
+
+pub fn partial<I, O>(mut parser: impl Parser<I, O>, after: usize) -> impl Parser<I, O> {
+  move |context: &mut ParseContext<I>| {
+    let initial_position = context.position;
+    match parser.parse(context) {
+      Err(NoMatch(err_position)) if err_position - initial_position > after => Err(PartialMatch(err_position)),
+      other => other
+    }
+  }
+}
+
+pub fn or<I, O>(mut first: impl Parser<I, O>, mut second: impl Parser<I, O>) -> impl Parser<I, O> {
+  move |context: &mut ParseContext<I>| {
+    let position = context.position;
+    match first.parse(context) {
+      Err(NoMatch(_)) => { 
+        context.position = position;
+        second.parse(context)
+      },
+      other => other
+    }
+  }
+}
+
+pub fn many<I, O>(mut parser: impl Parser<I, O>) -> impl Parser<I, Vec<O>> {
+  move |context: &mut ParseContext<I>| {
+    let mut outputs = Vec::new();
+    loop {
+      let position = context.position;
+      match parser.parse(context) {
+        Ok(output) => outputs.push(output),
+        Err(PartialMatch(err_position)) => return Err(PartialMatch(err_position)),
+        _ => {
+          context.position = position;
+          break
+        } 
+      }
+    }
+    Ok(outputs)
+  }
+}
+
+pub fn chains<I, O>(mut first: impl Parser<I, Vec<O>>, mut second: impl Parser<I, Vec<O>>) -> impl Parser<I, Vec<O>> {
+  move |context: &mut ParseContext<I>| {
+    let mut first_output = first.parse(context)?;
+    let second_output = second.parse(context)?;
+    first_output.extend(second_output);
+    Ok(first_output)
+  }
+}
+
+pub fn first<I, F, S>(mut first: impl Parser<I, F>, mut second: impl Parser<I, S>) -> impl Parser<I, F> {
+  move |context: &mut ParseContext<I>| {
+    let first_output = first.parse(context)?;
+    second.parse(context)?;
+    Ok(first_output)
+  }
+}
+
+pub fn second<I, F, S>(mut first: impl Parser<I, F>, mut second: impl Parser<I, S>) -> impl Parser<I, S> {
+  move |context: &mut ParseContext<I>| {
+    first.parse(context)?;
+    second.parse(context)
+  }
+}
+
+pub fn just<I, O>(mut make: impl FnMut() -> O + Clone) -> impl Parser<I, O> {
+  move |_: &mut ParseContext<I>| {
+    Ok(make()) 
+  }
+}
---
+++ src/lib.rs
@@ -0,0 +1,128 @@
+mod core;
+
+pub use core::*;
+
+#[macro_export]
+macro_rules! or {
+  ($parser:expr) => { $parser };
+  ($parser:expr, $($rest:expr),+) => { or($parser, or!($($rest),+)) };
+}
+
+#[macro_export]
+macro_rules! chains {
+  ($parser:expr) => { $parser };
+  ($parser:expr, $($rest:expr),+) => { chains($parser, chains!($($rest),+)) };
+}
+
+pub fn eq<I: Clone + PartialEq>(model: I) -> impl Parser<I, I> {
+  satisfy(move |i: &I| *i == model)
+}
+
+pub fn to_vec<I, O>(parser: impl Parser<I, O>) -> impl Parser<I, Vec<O>> {
+  map(parser, |output| vec![output])
+}
+
+pub fn many1<I, O>(parser: impl Parser<I, O>) -> impl Parser<I, Vec<O>> {
+  chains(to_vec(parser.clone()), many(parser))
+}
+
+pub trait ExtParser<I, O>: Parser<I, O> {
+  fn to_vec(self) -> impl Parser<I, Vec<O>> {
+    to_vec(self)
+  }
+
+  fn many1(self) -> impl Parser<I, Vec<O>> {
+    many1(self)
+  }
+}
+
+impl<I, O, P> ExtParser<I, O> for P where P: Parser<I, O> {}
+
+pub trait VecParser<I, O>: Parser<I, Vec<O>> {
+  fn chains(self, second: impl Parser<I, Vec<O>>) -> impl Parser<I, Vec<O>> {
+    chains(self, second)
+  }
+}
+
+pub fn eqchar(model: char, case_sensitive: bool) -> impl Parser<char, char> {
+  satisfy(move |c: &char| {
+    if case_sensitive {
+      *c == model
+    } else {
+      c.to_ascii_lowercase() == model.to_ascii_lowercase()
+    }
+  })
+}
+
+pub fn eqstr<S: AsRef<str>>(model: S, case_sensitive: bool) -> impl Parser<char, Vec<char>> {
+  let chars: Vec<char> = model.as_ref().chars().collect();
+  move |context: &mut ParseContext<char>| {
+    let mut output = Vec::new();
+    for &model in &chars {
+      output.push(eqchar(model, case_sensitive).parse(context)?);
+    }
+    Ok(output)
+  }
+}
+
+pub fn string<S: AsRef<str>>(model: S) -> impl Parser<char, Vec<char>> {
+  eqstr(model, true)
+}
+
+pub fn flatten<I, O>(mut parser: impl Parser<I, Vec<Vec<O>>>) -> impl Parser<I, Vec<O>> {
+  move |context: &mut ParseContext<I>| {
+    let mut output = Vec::new();
+    let intermediates = parser.parse(context)?;
+    for intermediate in intermediates {
+      output.extend(intermediate);
+    }
+    Ok(output)
+  }
+}
+
+pub trait FlattenParser<I, O>: Parser<I, Vec<Vec<O>>> {
+  fn flatten(self) -> impl Parser<I, Vec<O>> {
+    flatten(self)
+  }
+}
+
+impl<I, O, P> FlattenParser<I, O> for P where P: Parser<I, Vec<Vec<O>>> {}
+
+pub fn join<I>(mut first: impl Parser<I, String>, mut second: impl Parser<I, String>) -> impl Parser<I, String> {
+  move |context: &mut ParseContext<I>| {
+    let mut first_string = first.parse(context)?;
+    let second_string = second.parse(context)?;
+    first_string.push_str(&second_string);
+    Ok(first_string)
+  }
+}
+
+#[macro_export]
+macro_rules! join {
+  ($parser:expr) => { $parser };
+  ($parser:expr, $($rest:expr),+) => {
+    join($parser, join!($($rest),+))
+  };
+}
+
+pub fn to_string<I>(parser: impl Parser<I, Vec<char>>) -> impl Parser<I, String> {
+  parser.map(|output| output.into_iter().collect())
+}
+
+pub trait StringParser<I>: Parser<I, Vec<char>> {
+  fn to_string(self) -> impl Parser<I, String> {
+    to_string(self)
+  }
+}
+
+impl<I, P> StringParser<I> for P where P: Parser<I, Vec<char>> {}
+
+pub fn parse<I, O>(input: &[I], mut parser: impl Parser<I, O>) -> Result<O, ParseError> {
+  let mut context = ParseContext::new(input);
+  parser.parse(&mut context)
+}
+
+pub fn parse_str<S: AsRef<str>, O>(input: S, parser: impl Parser<char, O>) -> Result<O, ParseError> {
+  let chars: Vec<char> = input.as_ref().chars().collect();
+  parse(&chars, parser)
+}
