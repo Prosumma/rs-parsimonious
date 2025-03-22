4b134a222806664bafe4f87c4c4ede73cff19851
Gregory Higley <greg@prosumma.com>
2025-03-22 19:12:22 -0400

    Added test for invalid JSON

--- tests/json_test.rs
+++ tests/json_test.rs
@@ -13,3 +13,12 @@ fn json_sanity_check() {
   println!("{:?}", output);
   assert!(output.is_ok())
 }
+
+#[test]
+fn test_invalid_json() {
+  let path = Path::new("tests/Invalid.json");
+  let contents = fs::read_to_string(path).expect("JSON file is missing.");
+  let output = parse_str(contents, json.end());
+  println!("{:?}", output);
+  assert_eq!(output, Err(ParseError{ reason: NoMatch, position: 302, partial: true, extra: Some(JSONTokenError { token: JSONToken::Array, start: 288 }) }))
+}
